3eb38a5433ea8467ffbaa2ecea84f224c60fffe3
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-28 13:52:27 -0700

    Check for existing user in signup route

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,15 @@
+const User = require('../models/user');
+
 exports.signup = (req, res, next) => {
-  res.send({ success: 'true' });
+  const { email, password } = req.body;
+
+  // See if a user with given email exists
+  User.findOne({ email, password }, (err, existingUser) => {
+    // If a user with email does exist, return error
+
+  });
+
+  // If a user with email does not exist, create and save user record
+
+  // Respond to request
 };
