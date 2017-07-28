cee1df90c26ec57d925ee4ca14ef1727b1e66683
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-28 13:54:07 -0700

    Respond with 422 to existing users

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -5,8 +5,12 @@ exports.signup = (req, res, next) => {
 
   // See if a user with given email exists
   User.findOne({ email, password }, (err, existingUser) => {
-    // If a user with email does exist, return error
+    if (err) { return next(err); }
 
+    // If a user with email does exist, return error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use '});
+    }
   });
 
   // If a user with email does not exist, create and save user record
