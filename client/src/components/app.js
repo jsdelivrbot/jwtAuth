859eb6b8b07c6d67ae6f6b9f52f71f2dff9c36ea
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-31 13:22:42 -0700

    Add /signin route, connect signin form to redux form

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Header from './header';
+import Signin from './auth/signin';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <div>Hello from react!</div>
+        <Route path='/signin' component={Signin} />
       </div>
     );
   }
