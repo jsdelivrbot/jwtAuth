917f6d897aec368e54df3def3246a9e0a8de7b32
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-31 12:12:44 -0700

    Start setting up localStrategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,14 +3,30 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
-// Configure JwtStrategy
+// Create local strategy
+const localOptions = {
+  usernameField: 'email'
+};
+
+const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
+  // Verify this email and password
+  User.findOne({ email }, (err, user) => {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
+
+    // use bcrypt to compare passwords
+  });
+});
+
+
+// Create JWT strategy
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: config.secret
 };
 
-// Create JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
   // Check if subject of jwt (user ID) is in the db
   User.findById(payload.sub, (err, user) => {
