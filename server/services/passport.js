c20c78647d541eaed188ea7429860915e611e4dd
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-31 11:52:51 -0700

    Start setting up jwtStrategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -2,3 +2,18 @@ const passport = require('passport');
 const User = require('../models/user');
 const config = require('../config');
 const { JwtStrategy, ExtractJwt } = require('passport-jwt');
+
+const jwtOptions = {};
+
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+  // See if payload.user_id is in db
+  User.findById(payload.sub, (err, user) => {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done (null, false);
+    }
+  });
+});
