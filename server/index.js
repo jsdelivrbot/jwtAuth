f5eb3a07f815c907b7ce9359400ccbb9a023b06e
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-31 15:55:44 -0700

    Allow API to receive all CORS requests

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost/jwtauth', {
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost/jwtauth', {
 
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
