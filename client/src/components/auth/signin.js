8c51a8a059092b45fe838523d451b317041523c7
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-31 13:16:52 -0700

    Scaffold signin form

--- client/src/components/auth/signin.js
+++ client/src/components/auth/signin.js
@@ -3,7 +3,19 @@ import { reduxForm } from 'redux-form';
 
 class Signin extends Component {
   render() {
-    
+    return(
+      <form>
+        <fieldset className='form-group'>
+          <label>Email:</label>
+          <input className='form-control' />
+        </fieldset>
+        <fieldset className='form-group'>
+          <label>Email:</label>
+          <input className='form-control' />
+        </fieldset>
+        <button action='submit' className='btn btn-primary'>Sign in</button>
+      </form>
+    );
   }
 }
 
