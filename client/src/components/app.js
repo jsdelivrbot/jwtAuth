67a1e38ddeafad38d6ec7df7c9a35e54b5750e69
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-31 17:29:24 -0700

    Start on signup form

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Header from './header';
 import Signin from './auth/signin';
 import Signout from './auth/signout';
+import Signup from './auth/signup';
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
       <div>
         <Header />
         <Route path='/signin' component={Signin} />
+        <Route path='/signup' component={Signup} />
         <Route path='/signout' component={Signout} />
       </div>
     );
---
+++ client/src/components/auth/signup.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import { withRouter } from 'react-router-dom';
+
+class Signup extends Component {
+  handleFormSubmit({ email, password }) {
+    this.props.signupUser({ email, password })
+      .then(() => this.props.history.push('/feature'));
+  }
+
+  renderError() {
+    if (this.props.errorMessage) {
+      return (
+        <div className='alert alert-danger'>
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return(
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        signup
+        <fieldset className='form-group'>
+          <label>Email:</label>
+          <Field
+            name="email"
+            type="email"
+            component="input"
+            className='form-control' />
+        </fieldset>
+        <fieldset className='form-group'>
+          <label>Password:</label>
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            className='form-control' />
+        </fieldset>
+        {this.renderError()}
+        <button action='submit' className='btn btn-primary'>Sign in</button>
+      </form>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  errorMessage: state.auth.error
+});
+
+export default reduxForm({ form: 'signup' })(
+  connect(mapStateToProps, actions)(
+    withRouter(Signup)));
