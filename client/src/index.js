acace4ae9696b789166bf1c8fbe2425e420dbf4c
Nikita Shalimov <ndshalimov@gmail.com>
2017-08-02 19:46:05 -0700

    Setup persistent sessions by checking if jwt token is in localStorage before ReactDOM.render()

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,21 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = window.localStorage.getItem('token');
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <Route path='/' component={App} />
     </BrowserRouter>
