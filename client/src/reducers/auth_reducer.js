218944f1e564184766c1b0039570d19f231e034c
Nikita Shalimov <ndshalimov@gmail.com>
2017-08-02 19:23:34 -0700

    Clear auth errors on login

--- client/src/reducers/auth_reducer.js
+++ client/src/reducers/auth_reducer.js
@@ -8,7 +8,7 @@ export default (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case AUTH_USER:
-      return { ...state, authenticated: true };
+      return { ...state, error: null, authenticated: true };
     case UNAUTH_USER:
       return { ...state, authenticated: false };
     case AUTH_ERROR:
